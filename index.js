5b1da3de7d69505bf27d6407367d314b28e9501f
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2024-01-23 15:04:42 -0600

    APCALENDAR-vid374: Configuración variables de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from 'express';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 
 const app = express();
+const port = process.env.PORT;
 
+//carpeta publica
+app.use( express.static('public') )
 
-//crear servidor de express
-
-app.listen(4000, () => {
-    console.log("servidor corriendo en el puerto 4000");
+//*crear servidor de express
+app.listen(port, () => {
+    console.log(`Seridor corriendo en puerto: ${port}`);
     }
 );
 
-//rutas
+//*rutas
 
 app.get("/", (req, res) => {
     res.status(200).json({
