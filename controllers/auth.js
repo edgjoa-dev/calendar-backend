ee92456e3a61036ca7c7db70b2f30bc2f59b90fa
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-02-26 20:31:54 -0600

    Dev - VID:383 : Validar si existe email al registrar un nuevo usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,18 @@ import { request, response } from 'express';
 import User from '../models/User.js';
 
 export const createUser = async(req=request, res=response) => {    
+
+    const { email } = req.body;
+    //Válida si el usuario ya existe en la base de datos
     try {
+        let user = await User.findOne({ email });
+        
+        if( user ){
+            return res.status(400).json({
+                msg: 'El usuario ya existe'
+            });
+        }
+
     //Guardar newUser en base de datos de mongodb
         const newUser = new User(req.body);
 
@@ -10,9 +21,12 @@ export const createUser = async(req=request, res=response) => {
 
         res.status(201).json({
             msg: 'Usuario creado ok!',
+            uid: newUser.id,
+            name: newUser.name,
         })
         
     } catch (error) {
+        //Mostrar error de registro nuevo usuario
         console.log(error);
         return res.status(500).json({
             msg: 'Error en el servidor, porfavor contacte al administrador'
