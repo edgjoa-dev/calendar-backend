d0ecf986847b817779287872c8ec4510908d0f8d
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-03-11 20:14:11 -0600

    VID 397 - Obtener el listado de los eventos

--- controllers/events.js
+++ controllers/events.js
@@ -1,27 +1,40 @@
 import { request, response } from "express";
+import Event  from "../models/Events.js";
 
 
-export const getEvent = ( req=request, res=response )=> {
 
-    return res.status(200).json({
-        ok:true,
-        msg: 'getEvent - ok'
-    })
 
+export const getEvent = async( req=request, res=response )=> {
+
+    const events = await Event.find().populate('user', 'name');
+
+    try {
+        return res.status(200).json({
+            ok:true,
+            events
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador o revise los logs en consola'
+        })
+    }
 }
 
 
 export const createEvent = async( req=request, res=response )=> {
 
     const event = new Event(req.body);
+    console.log(event);
 
     try {
-
+        event.user = req.uid;
         const eventCreated = await event.save();
 
         return res.status(200).json({
             ok:true,
-            event: eventCreated
+            eventCreated
         })
 
     } catch (error) {
